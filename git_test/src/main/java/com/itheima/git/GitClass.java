b391a2d3ee127a3d57222b4ee088c09870d5092f
xiaobin <1132580876@qq.com>
2019-06-17 20:45:31 +0800

    提交一个java工程

---
+++ git_test/src/main/java/com/itheima/git/GitClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class GitClass {
+    public static void main(String[] args) {
+
+        System.out.println("===========");
+        System.out.println("===========");
+        System.out.println("===========");
+        System.out.println("===========");
+        System.out.println("===========");
+    }
+}
